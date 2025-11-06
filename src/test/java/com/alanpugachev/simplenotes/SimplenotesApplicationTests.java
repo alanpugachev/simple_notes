48350ce9aa9436511b2996eeaf6b97a3e7f2a300
Alan Pugachev <aszneesizzle@gmail.com>
2025-11-06 14:35:51 +0300

    init

---
+++ src/test/java/com/alanpugachev/simplenotes/SimplenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpugachev.simplenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
